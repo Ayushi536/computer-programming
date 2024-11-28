de105695beb82ae495dcad36ab61fbdad1610202
Ayushi536 <ayu704sharma32@gmail.com>
2024-11-28 11:34:52 +0530

    list_q13.py

---
+++ coding-assignment/list/list_q13.py
@@ -0,0 +1,9 @@
+"""
+ques.13 :  Sum of List Elements
+Write a program to calculate the sum of all elements of a list using a for loop.
+"""
+numbers = [1, 2, 3, 4, 5]
+sum = 0
+for num in numbers:
+    sum += num
+print(sum)
