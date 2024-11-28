1b2b94946e2dc5e8cda8e7fc1b51b88170f4a304
Ayushi536 <ayu704sharma32@gmail.com>
2024-11-28 11:59:47 +0530

    function_q06.py

---
+++ coding-assignment/function/function_q06.py
@@ -0,0 +1,9 @@
+"""
+ques.6 : Write a function to print all numbers from 1 to n.
+"""
+def print_numbers(n):
+    for i in range(1, n + 1):
+        print(i, end=" ")
+    print()
+
+print_numbers(10) 
