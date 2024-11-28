19869e68f667f440a77c2807f64e29c297b0841d
Ayushi536 <ayu704sharma32@gmail.com>
2024-11-28 11:47:05 +0530

    tuple_q05.py

---
+++ coding-assignment/tuple/tuple_q05.py
@@ -0,0 +1,9 @@
+"""
+ques.5 : Calculate the sum of all elements in a tuple
+Question: Write a program to calculate the sum of (1, 2, 3, 4, 5) using a loop.
+"""
+tpl = (1, 2, 3, 4, 5)
+total = 0
+for num in tpl:
+    total += num
+print(total)
